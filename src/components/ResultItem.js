61553e247cdd6fed11fbc1c7c003dc7de0f92717
Risyardi Iswanto <elyeel@yahoo.com>
2020-08-30 18:17:55 -0700

    changing ul to article

--- src/components/ResultItem.js
+++ src/components/ResultItem.js
@@ -1,27 +1,27 @@
-import React, {useState} from "react";
-import NominateBtn from './NominateBtn';
+import React, { useState } from "react";
+import NominateBtn from "./NominateBtn";
 
 export default function ResultItem(props) {
   const [nominateBtnDisabled, setNominateBtnDisabled] = useState(false);
 
   return (
     <li>
-      <div className='result_item'>
-      <h3>
-        <span>
-          <img src={props.poster} alt="Poster"></img>
-        </span>
-        {props.title} ({props.year})
-        <NominateBtn 
-          nominee={props.nominee}
-          setNominee={props.setNominee}
-          poster={props.poster}
-          title={props.title}
-          year={props.year}
-          nominateBtnDisabled={nominateBtnDisabled}
-          setNominateBtnDisabled={setNominateBtnDisabled}
-        />
-      </h3>
+      <div className="result_item">
+        <h3>
+          <span>
+            <img src={props.poster} alt="Poster"></img>
+          </span>
+          {props.title} ({props.year})
+          <NominateBtn
+            nominee={props.nominee}
+            setNominee={props.setNominee}
+            poster={props.poster}
+            title={props.title}
+            year={props.year}
+            nominateBtnDisabled={nominateBtnDisabled}
+            setNominateBtnDisabled={setNominateBtnDisabled}
+          />
+        </h3>
       </div>
     </li>
   );
